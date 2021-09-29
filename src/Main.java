38d8e1ef175ad7a2c82a00897d930fe495413cd3
Kinga Kasicka <kinga kasicka>
2021-09-29 10:47:57 +0100

    Pierwsze zadanie

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,16 @@ public class Main {
           Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indexu (podać nr indexu)
           to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
         */
-        int i=1000;
-        int indeks=57071;
-        int n=indeks%100;
+        int i = 1000;
+        int indeks = 57071;
+        int n = indeks % 100;
         System.out.println("\n");
-       do{
-           System.out.println( i);
-           i--;
-           if(i%n==0)
-               System.out.println("Liczba" +" "+ i +" "+ "podzielna przez" + " " +n);
-       }while(i>=0);
+        do {
+            System.out.println(i);
+            i--;
+            if (i % n == 0)
+                System.out.println("Liczba" + " " + i + " " + "podzielna przez" + " " + n);
+        } while (i >= 0);
 
         /* zad 2
         a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
@@ -25,11 +25,21 @@ public class Main {
         b) jak wyżej tylko dla tablicy typu double[],
         c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
         */
+        int[] tab = new int[]{0, 3, 6, 87, 9};
 
+        int j = 0;
+        while (j < 5) {
+            System.out.println("tab[j]:" + tab[j]);
+            j++;
+        }
         /* zad.3
         a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
         b) a jak gdy damy false --> while(false)
         */
+        while (true) {
+            System.out.println("Jesteśmy w domu");
+        }
 
     }
+    /*Przy pętli while(false)wystąpi błąd*/
 }
