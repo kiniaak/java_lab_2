7941bf3d3285722c391e270fbeb076662c67aafa
Kinga Kasicka <kinga kasicka>
2021-09-29 14:04:21 +0100

    Trzecie zadanie

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,22 @@ public class Main {
           a) ta liczba jest parzysta --> podpowiedz switch(a % 2) {
           b) program który określi czy liczba jest podzielna przez 7
          */
+           Scanner scan=new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+           int num= scan.nextInt();
+           switch(num){
+               case 1:
 
+                      System.out.println((num%2==0)+"Liczba jest parzysta");
+
+                  break;
+               case 2:
+                   if(num%7==0)
+                   {
+                       System.out.println("Liczba jest podzielna przez 7");
+                   }
+
+           }
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
