17451bf6c2be87205b673b0b135daa782f612c4a
Kinga Kasicka <kinga kasicka>
2021-09-29 11:22:36 +0100

    Drugie zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,11 +9,22 @@ public class Main {
         /* zad.1 Napisz program który będzie iterował od 100 do 0,
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Podaj numer indeksu");
+        int indeks=scan.nextInt();
+        for (int i = 0; i <=100 ; i++) {
+            if (i % (indeks % 10) == 0) {
+                System.out.println("Liczba podzielna przez ostatnią cyfrę mojego indeksu:" + i);
+            } else if (indeks%10==0) {
+                System.out.println("Liczba podzielna przez dwie ostatnie cyfry mojego indeksu:" + i % (indeks % 100) == 0);
+            }
+        }
 
+        }
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
 
     }
-}
+
